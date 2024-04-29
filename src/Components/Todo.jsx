91493fbab115f878669c038821e213dcadae5aaa
FernandoFernandesFarias <fariasfernando028@gmail.com>
2024-04-28 22:37:27 -0300

    Finalizando o projeto

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,23 +1,22 @@
 import React, { useState } from 'react'
-import { FaTrash } from "react-icons/fa";
-
+import { FaTrash } from 'react-icons/fa'
 
 const Todo = () => {
   const [newTask, setNewTask] = useState('')
   const [tasks, setTasks] = useState([])
 
-  const handleTaskChange = (e) => {
+  const handleTaskChange = e => {
     setNewTask(e.target.value)
   }
 
-  const addTask = (e) => {
-    if (e.keyCode === 13 && newTask.trim() != '') {
+  const addTask = e => {
+    if (e.keyCode === 13 && newTask.trim() !== '') {
       setTasks(t => [...t, newTask])
       setNewTask('')
     }
   }
 
-  const deleteTask = (index) => {
+  const deleteTask = index => {
     const updateTasks = tasks.filter((_, i) => i !== index)
     setTasks(updateTasks)
   }
@@ -25,21 +24,30 @@ const Todo = () => {
   return (
     <div className="container">
       <div className="todo-list">
-        <h1>Adicione suas tarefas</h1>
+        <h1>TodoList</h1>
         <div className="todo-task">
-          <input onKeyDown={addTask} value={newTask} onChange={handleTaskChange} placeholder='Pressione enter para adicionar'/>
+          <input
+            onKeyDown={addTask}
+            value={newTask}
+            onChange={handleTaskChange}
+            placeholder="Pressione enter para adicionar"
+          />
         </div>
 
-        <div className="todos">
-          <ol>
-            {tasks.map((task, index) => 
-              <li key={index}>
-                <span>{task}</span>
-                <button  onClick={() => deleteTask(index)}><FaTrash/></button>
-              </li>
-          )}
-          </ol>
-        </div>
+        {tasks.length > 0 && (
+          <div className="todos">
+            <ol>
+              {tasks.map((task, index) => (
+                <li key={index}>
+                  <span>{task}</span>
+                  <button onClick={() => deleteTask(index)}>
+                    <FaTrash />
+                  </button>
+                </li>
+              ))}
+            </ol>
+          </div>
+        )}
       </div>
     </div>
   )
